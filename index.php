ee420bf9c84c75d68769b9714af68692c6fd5cd0
Ian Hamilton <ian@colorcrate.com>
2012-06-21 16:34:11 -0400

    index.php example updates
    Added some color coded visual examples to index.php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <html class="<?php echo css_browser_selector() ?>">
 <head>
 <title>PHP CSS Browser Selector Example</title>
+<style type='text/css'>
+	.box { margin: 0 0 10px 0; height: 100px; width: 100px; }
+	
+	/* CSS below provides examples of how to use selectors */
+	.ie .box { background: red; }
+	.ff .box { background: blue; }
+	.chrome .box { background: green; }
+	.safari .box { background: orange; }
+	.opera .box { background: cyan; }
+	
+	
+	span { padding: 5px; color: #fff; width: 300px; }
+</style>
 </head>
 <body>
 	<h1>PHP CSS Browser Selector Example</h1>
@@ -12,5 +25,12 @@
 		<li>???</li>
 		<li><b>Enjoy</b> using browser-specific CSS selectors!</li>
 	</ol>
+	<p>The box below will change colors depending on what browser you're on.</p>
+	<div class="box"></div>
+	<span style="background: red;">Internet Explorer is red.</span>
+	<span style="background: blue;">Firefox is blue.</span>
+	<span style="background: green;">Chrome is green.</span>
+	<span style="background: orange;">Safari is orange.</span>
+	<span style="background: cyan;">Opera is <s>silly</s> cyan.</span>
 </body>
 </html>
