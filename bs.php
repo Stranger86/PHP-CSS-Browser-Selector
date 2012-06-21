f7378be660a69a1f011e6615ff194414e1d8ce67
Ian Hamilton <ian@ian.local>
2012-06-21 16:21:50 -0400

    To do list + updated version
    Added a short to-do list to remind myself what needs to be fixed in the
    future. Also updated the css_browser_selector function to the most
    recent version (supports up to FF 16).

--- bs.php
+++ bs.php
@@ -6,6 +6,10 @@ Ian Hamilton (http://colorcrate.com)
 http://bastian-allgeier.de/css_browser_selector
 License: http://creativecommons.org/licenses/by/2.5/
 Credits: This is a PHP port of Rafael Lima's original Javascript CSS Browser Selector (http://rafael.adm.br/css_browser_selector). The port was created by Bastian Allgeier (http://bastian-allgeier.de), however he stopped updating the plugin in favor of his PHP toolkit, Kirby (http://getkirby.com/). Ian Hamilton (http://colorcrate.com) took over, adding support for numerous versions of Firefox and currently maintains the port.
+
+To Do:
+- Fix Chrome error (incorrectly adds "safari" to the string)
+- Fix Firefox so it doesn't need to be updated every time they release a new version
 */
 
 function css_browser_selector($ua=null) {
@@ -31,8 +35,26 @@ function css_browser_selector($ua=null) {
 				$b[] = $g . ' ff5';
 		}	else if(strstr($ua, 'firefox/6.0')) {
 				$b[] = $g . ' ff6';
-		}	else if(strstr($ua, 'firefox/7.0')) {
-				$b[] = $g . ' ff7';
+		}	else if(strstr($ua, 'firefox/9.0')) {
+				$b[] = $g . ' ff9';
+		}	else if(strstr($ua, 'firefox/8.0')) {
+				$b[] = $g . ' ff8';
+		}	else if(strstr($ua, 'firefox/9.0')) {
+				$b[] = $g . ' ff9';
+		}	else if(strstr($ua, 'firefox/10.0')) {
+				$b[] = $g . ' ff10';
+		}	else if(strstr($ua, 'firefox/11.0')) {
+				$b[] = $g . ' ff11';
+		}	else if(strstr($ua, 'firefox/12.0')) {
+				$b[] = $g . ' ff12';
+		}	else if(strstr($ua, 'firefox/13.0')) {
+				$b[] = $g . ' ff13';
+		}	else if(strstr($ua, 'firefox/14.0')) {
+				$b[] = $g . ' ff14';
+		}	else if(strstr($ua, 'firefox/15.0')) {
+				$b[] = $g . ' ff15';
+		}	else if(strstr($ua, 'firefox/16.0')) {
+				$b[] = $g . ' ff16';
 		}	else if(strstr($ua, 'firefox/3')) {
 				$b[] = $g . ' ff3';
 		} else if(strstr($ua, 'gecko/')) {
